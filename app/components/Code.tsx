acf7eed2e55fffdb6e94c0556408e97900c784ab
Liul Alemayehu <liulalemx@gmail.com>
2023-05-27 14:21:37 +0300

    Added framer

--- app/components/Code.tsx
+++ app/components/Code.tsx
@@ -1,30 +1,38 @@
+"use client"
+
 import Image from "next/image"
 import lexSnip from "../../public/lexSnip.png"
 import stpSnip from "../../public/stpSnip.png"
 import transSnip from "../../public/transSnip.png"
 import stemSnip from "../../public/stemSnip.png"
-
+import { motion } from "framer-motion"
 type Props = {
   type: string
 }
 
 const Code = ({ type }: Props) => {
   return (
-    <Image
-      src={
-        type === "lex"
-          ? lexSnip
-          : type === "stp"
-          ? stpSnip
-          : type === "trans"
-          ? transSnip
-          : stemSnip
-      }
-      width={1000}
-      height={1000}
-      className="w-auto h-auto"
-      alt=""
-    />
+    <motion.div
+      initial={{ opacity: 0, y: 300 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >
+      <Image
+        src={
+          type === "lex"
+            ? lexSnip
+            : type === "stp"
+            ? stpSnip
+            : type === "trans"
+            ? transSnip
+            : stemSnip
+        }
+        width={1000}
+        height={1000}
+        className="w-auto h-auto"
+        alt=""
+      />
+    </motion.div>
   )
 }
 
